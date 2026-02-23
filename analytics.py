b96aadf32e064177e028fc2fa591b49e4f45eb15
jyu26 <jyu26@bentleyschool.org>
2026-02-23 09:03:22 -0800

    Add files via upload

--- analytics.py
+++ analytics.py
@@ -20,3 +20,38 @@ def filter_threshold(numbers, limit):
         if n > limit:
             filtered_results.append(n)
     return filtered_results
+
+
+
+def binarySearch(items:list, target) -> tuple[int,int]:
+    # Modify the below function such that it implements linear search.
+    # Return the index of the target value and the amount of checks it took
+    # if the value is not within the list return -1 as the index.
+    cnt = 0
+    l = 0
+    r = len(items)-1
+
+    while(True):
+        cnt+=1
+        mid = l+ int((r-l)/2)
+        if items[mid]== target:
+            return mid, cnt
+        elif items[mid] < target:
+            l=mid+1
+        else:
+            r=mid-1
+        if r < l:
+            return -1,cnt
+
+
+
+def linearSearch(items:list, target) ->tuple[int,int]:
+    #Modify the below function such that it implements linear search.
+    #Return the index of the target value and the amount of checks it took
+    #if the value is not within the list return -1 as the index.
+
+    for i in range(0, len(items)):
+        if items [i] == target:
+            return i, i+1
+
+    return -1, i+1
--- test_CSP_6_01_Library_Basics.py
+++ test_CSP_6_01_Library_Basics.py
@@ -7,7 +7,7 @@ def test_analyze_score():
     assert HW.analyze_scores([2,4,6,9,8,4,2]) == (9,5.0)
 
 def test_sanitize_usernames():
-    assert HW.santize_usernames(["hi "," bye ","Hello","apples","Tree","Hilarious"]) == (["hi", "bye", "hello", "apples", "tree", "hilarious"])
+    assert HW.sanitize_usernames(["hi "," bye ","Hello", "apples", "Tree", "Hilarious"]) == (["hi","bye","hello", "apples", "tree", "hilarious"])
 
 def test_identify_outliers():
     assert HW.identify_outliers([5, 1200, 99, 100, -20, 4.5, 200, 247]) == ([1200, 200, 247])
